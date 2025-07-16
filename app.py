cc9fc4e14d71de2ff6dffaefbb72d4bf37a00f8e
itsleo4 <bholanitin308@gmail.com>
2025-07-16 09:53:33 +0530

    Fix: Render-compatible port binding

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
@@ -24,4 +25,5 @@ def file():
     return render_template('file.html')  # This will contain the final download link
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render sets this automatically
+    app.run(host='0.0.0.0', port=port)
